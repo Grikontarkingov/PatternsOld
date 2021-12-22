6ebcd03f32a4e89ddc8ee6b5dcd1cc8e63a8df26
Shini <kerlaeda3@yandex.ru>
2021-12-22 16:41:56 +0300

    LessonOne

---
+++ LessonOne/include/FileLoggerSingleton.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <chrono>
+
+class ILogger{
+public:
+    virtual ~ILogger() = default;
+
+    virtual void OpenLogFile(const std::string& FN) = 0;
+
+    virtual void CloseLogFile() = 0;
+
+    virtual void WriteToLog(const std::string& str) = 0;
+
+    virtual void WriteToLog(const std::string& str, int n) = 0;
+
+    virtual void WriteToLog(const std::string& str, double d) = 0;
+
+    virtual std::string GetCurDateTime() = 0;
+};
+
+class FileLoggerSingleton : public ILogger{
+public:
+    static ILogger& getInstance();
+
+    virtual void OpenLogFile(const std::string& FN) override;
+
+    virtual void CloseLogFile() override;
+
+    virtual void WriteToLog(const std::string& str) override;
+
+    virtual void WriteToLog(const std::string& str, int n) override;
+
+    virtual void WriteToLog(const std::string& str, double d) override;
+
+private:
+    friend ILogger& getInternalInstanceLogger();
+
+    std::ofstream logOut;
+
+    FileLoggerSingleton(){};
+    FileLoggerSingleton(const FileLoggerSingleton& root) = delete;
+    FileLoggerSingleton& operator=(const FileLoggerSingleton&) = delete;
+    virtual std::string GetCurDateTime() override;
+
+};
+
+
